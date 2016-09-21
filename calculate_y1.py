e9ed15a222981bb0bd9e43f06601fdff84d6093b
Michael Smith <michael.smith6@mail.mcgill.ca>
2016-09-20 20:53:55 -0400

    Working logistic regression

--- calculate_y1.py
+++ calculate_y1.py
@@ -1,13 +1,28 @@
 import numpy as np
-import logistic_regression
+import log_reg
 
-data = np.loadtxt('logistic_test.txt')
+# Tests against a toy dataset
 
-X = data[:, 0:2]
+# TODO: Add cross validation
 
-y = data[:,2].reshape(-1,1)
+data = np.genfromtxt('toy.csv', delimiter=',')
 
-log_reg = logistic_regression.lg()
+X_train = data[:70, :2]
+X_test = data[-30:, :2]
+y_train = data[:70, 2]
+y_test = data[-30:, 2]
 
-log_reg.fit(X,y)
+log_reg = log_reg.lg(X_train, y_train)
+
+log_reg.fit()
+
+y_as_int = y_test.reshape((y_test.shape[0], -1)).astype(np.int32)
+
+result_as_int = log_reg.predict(X_test).astype(np.int32)
+
+accuracy = (np.count_nonzero(result_as_int == y_as_int) / y_as_int.size)*100
+
+print("Classification (test) accuracy: %f percent" % accuracy)
+
+
 
---
+++ log_reg.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+class lg:
+    def __init__(self, X, y, error_threshold = 1e-4, alpha=0.1, max_iterations = 10000):
+
+        # Define X : add ones column
+        self.X = np.insert(X, 0, 1, 1)
+
+        # Make sure y has correct shape
+        self.y = y.reshape((y.shape[0], -1))
+
+        self.w = np.zeros((self.X.shape[1], 1))
+
+        self.error_threshold = error_threshold
+
+        self.alpha = alpha
+
+        self.max_iterations = max_iterations
+
+
+    def fit(self):
+
+        diff = self.error_threshold + 1
+
+        old_likelihood = self.log_likelihood()
+        iteration = 0
+
+        while diff > self.error_threshold and iteration < self.max_iterations:
+            # Sum takes care of the fact that we are doing all X_i at once; reshape ensures correct output shape
+            self.w = self.w + self.alpha * ((self.y - self.sigmoid(self.X.dot(self.w))) * self.X).sum(0).reshape(self.w.shape)
+
+            cur_likelihood = self.log_likelihood()
+            diff = np.abs(cur_likelihood - old_likelihood)
+            old_likelihood = cur_likelihood
+            iteration += 1
+
+        print("Took %i iterations, difference = " % iteration, diff)
+
+
+    def sigmoid(self, x):
+        return 1/(1 + np.exp(-x))
+
+    def log_likelihood(self):
+
+        sig = self.sigmoid(self.X.dot(self.w))
+        # Define epsilon
+        epsilon = np.spacing(1)
+        return -(self.y * np.log(sig+epsilon) + (1-self.y) * np.log(1-sig+epsilon)).sum()
+
+
+    def predict(self, X):
+
+        X2 = np.insert(X, 0, 1, 1)
+        prod = self.sigmoid(X2.dot(self.w))
+        prod[prod < 0.5] = 0
+        prod[prod >= 0.5] = 1
+        return prod
